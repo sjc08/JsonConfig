4618e1943d4139e505f1dfa80f96b92ea74b0c8e
AlexSJC <cn_sjc@outlook.com>
2024-02-25 15:05:44 +0800

    Update.

---
+++ JsonConfig/JsonConfigOptions.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace AS.JsonConfig
+{
+
+    public sealed class JsonConfigOptions
+    {
+        public JsonConfigOptions() { }
+
+        public JsonConfigOptions(JsonConfigOptions options)
+        {
+            CreateNew = options.CreateNew;
+            SaveNew = options.SaveNew;
+            SerializerOptions = options.SerializerOptions;
+        }
+
+        public bool CreateNew { get; set; } = true;
+
+        public bool SaveNew { get; set; } = true;
+
+        public JsonSerializerOptions SerializerOptions { get; set; } = new()
+        {
+            AllowTrailingCommas = true,
+            Converters = { new JsonStringEnumConverter() },
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals | JsonNumberHandling.AllowReadingFromString,
+            PropertyNameCaseInsensitive = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            WriteIndented = true
+        };
+    }
+}
