850b3423d7bd466a7c7c420da9c3cc62f1079dc2
AlexSJC <cn_sjc@outlook.com>
2024-07-14 20:32:28 +0800

    update

--- JsonConfig/JsonConfig.cs
+++ JsonConfig/JsonConfig.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using static Asjc.Extensions.TryExtensions;
+using static Asjc.Extensions.TryHelper;
 
 namespace Asjc.JsonConfig
 {
