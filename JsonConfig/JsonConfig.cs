be657cb339aaa4291f729dd02b65fe39216c476c
AlexSJC <cn_sjc@outlook.com>
2024-07-16 23:05:59 +0800

    update

--- JsonConfig/JsonConfig.cs
+++ JsonConfig/JsonConfig.cs
@@ -64,12 +64,20 @@ namespace Asjc.JsonConfig
 
         protected virtual void OnAfterSave() => AfterSave?.Invoke(this);
 
+        /// <summary>
+        /// Load the config generally.
+        /// </summary>
+        /// <typeparam name="T">The type of config to be loaded.</typeparam>
+        /// <returns>The loaded config.</returns>
         public static T? Load<T>() where T : JsonConfig, new() => Load<T>(new T().DefaultPath, new T().DefaultOptions);
 
+        /// <inheritdoc cref="Load{T}()"/>
         public static T? Load<T>(string path) where T : JsonConfig, new() => Load<T>(path, new T().DefaultOptions);
 
+        /// <inheritdoc cref="Load{T}()"/>
         public static T? Load<T>(JsonConfigOptions options) where T : JsonConfig, new() => Load<T>(new T().DefaultPath, options);
 
+        /// <inheritdoc cref="Load{T}()"/>
         public static T? Load<T>(string path, JsonConfigOptions options) where T : JsonConfig, new()
         {
             T? config = default;
@@ -81,28 +89,58 @@ namespace Asjc.JsonConfig
             return config;
         }
 
+        /// <summary>
+        /// Try to load the config. This does not throw an exception.
+        /// </summary>
+        /// <typeparam name="T">The type of config to be loaded.</typeparam>
+        /// <param name="config">The loaded config.</param>
+        /// <returns>A <see langword="bool"/> indicating whether the load was successful.</returns>
         public static bool TryLoad<T>(out T? config) where T : JsonConfig, new() => Try(() => Load<T>(), out config);
 
+        /// <inheritdoc cref="TryLoad{T}(out T)"/>
         public static bool TryLoad<T>(string path, out T? config) where T : JsonConfig, new() => Try(() => Load<T>(path), out config);
 
+        /// <inheritdoc cref="TryLoad{T}(out T)"/>
         public static bool TryLoad<T>(JsonConfigOptions options, out T? config) where T : JsonConfig, new() => Try(() => Load<T>(options), out config);
 
+        /// <inheritdoc cref="TryLoad{T}(out T)"/>
         public static bool TryLoad<T>(string path, JsonConfigOptions options, out T? config) where T : JsonConfig, new() => Try(() => Load<T>(path, options), out config);
 
+        /// <summary>
+        /// Try to load the config. This does not throw an exception.
+        /// </summary>
+        /// <typeparam name="T">The type of config to be loaded.</typeparam>
+        /// <returns>The loaded config if successful; otherwise, <see langword="default"/>.</returns>
         public static T? TryLoad<T>() where T : JsonConfig, new() => Try(() => Load<T>());
 
+        /// <inheritdoc cref="TryLoad{T}()"/>
         public static T? TryLoad<T>(string path) where T : JsonConfig, new() => Try(() => Load<T>(path));
 
+        /// <inheritdoc cref="TryLoad{T}()"/>
         public static T? TryLoad<T>(JsonConfigOptions options) where T : JsonConfig, new() => Try(() => Load<T>(options));
 
+        /// <inheritdoc cref="TryLoad{T}()"/>
         public static T? TryLoad<T>(string path, JsonConfigOptions options) where T : JsonConfig, new() => Try(() => Load<T>(path, options));
 
+        /// <summary>
+        /// Load the config completely safely. This will never return <see langword="null"/>.
+        /// </summary>
+        /// <remarks>
+        /// Use this method with caution!
+        /// It replaces the read config with a new one if it is invalid, which can lead to data loss.
+        /// Unless more serious problems are likely to occur, consider taking care of it yourself.
+        /// </remarks>
+        /// <typeparam name="T">The type of config to be loaded.</typeparam>
+        /// <returns>The loaded config.</returns>
         public static T SafeLoad<T>() where T : JsonConfig, new() => TryLoad<T>() ?? Create<T>();
 
+        /// <inheritdoc cref="SafeLoad{T}()"/>
         public static T SafeLoad<T>(string path) where T : JsonConfig, new() => TryLoad<T>(path) ?? Create<T>(path);
 
+        /// <inheritdoc cref="SafeLoad{T}()"/>
         public static T SafeLoad<T>(JsonConfigOptions options) where T : JsonConfig, new() => TryLoad<T>(options) ?? Create<T>(options);
 
+        /// <inheritdoc cref="SafeLoad{T}()"/>
         public static T SafeLoad<T>(string path, JsonConfigOptions options) where T : JsonConfig, new() => TryLoad<T>(path, options) ?? Create<T>(path, options);
 
         public static T? Read<T>() where T : JsonConfig, new() => Read<T>(new T().DefaultPath, new T().DefaultOptions);
@@ -141,12 +179,18 @@ namespace Asjc.JsonConfig
             return config;
         }
 
+        /// <summary>
+        /// Save the current config to a file.
+        /// </summary>
         public void Save() => Save(Path, Options);
 
+        /// <inheritdoc cref="Save()"/>
         public void Save(string path) => Save(path, Options);
 
+        /// <inheritdoc cref="Save()"/>
         public void Save(JsonConfigOptions options) => Save(Path, options);
 
+        /// <inheritdoc cref="Save()"/>
         public void Save(string path, JsonConfigOptions options)
         {
             OnBeforeSave();
@@ -155,12 +199,19 @@ namespace Asjc.JsonConfig
             OnAfterSave();
         }
 
+        /// <summary>
+        /// Try to save the config. This does not throw an exception.
+        /// </summary>
+        /// <returns>A <see langword="bool"/> indicating whether the save was successful.</returns>
         public bool TrySave() => Try(() => Save());
 
+        /// <inheritdoc cref="TrySave()"/>
         public bool TrySave(string path) => Try(() => Save(path));
 
+        /// <inheritdoc cref="TrySave()"/>
         public bool TrySave(JsonConfigOptions options) => Try(() => Save(options));
 
+        /// <inheritdoc cref="TrySave()"/>
         public bool TrySave(string path, JsonConfigOptions options) => Try(() => Save(path, options));
 
         public override string ToString() => Json;
